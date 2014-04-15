f38d25530fa3c8f860120bfaf822859b6af58826
Stanislav Rogovskiy <stanislav.rogovskiy@gmail.com>
2014-04-15 18:13:18 +0300

    Rewritten in OOP style

--- javascripts/interference.js
+++ javascripts/interference.js
@@ -1,28 +1,25 @@
-var canvas, context;
-var size = 700;
+var context;
+var size = 500;
 var height = size, width = size;
 var center = [Math.floor(width/2), Math.floor(height/2)];
-var pixelSize = 2;
-var wavelength = 35;
 var period = 20;
 var momentsCount = 10000;
+var radius = 5;
 var sources;
-var startTime = 0, curTime = startTime;
-var diff = 0;
-var radius = 4;
-var step = 1;
-var S;
-var sourcesCount, maximumSources = 25;
-var interval = 50;
+var t = 0;
+var dt = 1;
+var sourcesCount, maximumSources = 20;
+var interval = 100;
+var fields;
 
 var running = true, showSources = true;
 
 function init() {
-	canvas = document.getElementById('canvas');
-	context = canvas.getContext('2d');
+	var canvas = document.getElementById('canvas');
 	canvas.width = width;
 	canvas.height = height;
 
+	context = canvas.getContext('2d');
 	context.fillStyle="white";
 	context.fillRect(0,0,width,height);
 
@@ -35,7 +32,7 @@ function init() {
 	}
 	select.options[3].selected = true;
 
-	changeSources();
+	setSources();
 
 	image = context.getImageData(0, 0, width, height);
 	run();
@@ -45,7 +42,7 @@ function createSources (n) {
 	sourcesCount = n;
 	sources = new Array(sourcesCount);
 	if (sourcesCount == 1) {
-		sources[0] = [width/2, height/2];
+		sources[0] = new Source(width/2, height/2, period, momentsCount, radius, sourcesCount);
 	}
 	else
 	{
@@ -53,46 +50,32 @@ function createSources (n) {
 
 		for (var i = sourcesCount-1; i>=0; --i) {
 			alpha = 2*Math.PI*i/sourcesCount;
-			sources[i] = [center[0] + Math.round(R*Math.sin(alpha)), center[1] - Math.round(R*Math.cos(alpha))];
+			sources[i] = new Source(center[0] + Math.round(R*Math.sin(alpha)),
+				center[1] - Math.round(R*Math.cos(alpha)), period, momentsCount, radius, sourcesCount);
 		}
 	}
-	S = calculateSinuses(period, momentsCount);
-	D = calculateDistances(sources);
-}
 
-function drawSources (sources, radius) {
-	var pixels = image.data;
+	fields = new Array(sourcesCount);
 	for (var i = sources.length - 1; i >= 0; i--) {
-		index = 4*(width*sources[i][1] + sources[i][0] - radius);
-		for (var j = 4*(2*radius-1); j>=0; j -= 4) {
-			pixels[index + j] = 255.0;
-	        pixels[index + j + 1] = 0;
-	        pixels[index + j + 2] = 0;
-	        pixels[index + j + 3] = pixels[index + j + 3];
-		}
-
-		index = 4*(width*(sources[i][1] - radius) + sources[i][0]);
-		for (var j = 4*width*(2*radius-1); j>=0; j -= 4*width) {
-			pixels[index + j] = 255.0;
-	        pixels[index + j + 1] = 0;
-	        pixels[index + j + 2] = 0;
-	        pixels[index + j + 3] = pixels[index + j + 3];
+		fields[i] = new Array(height);
+	    for (var y = height - 1; y >= 0; y--) {
+	    	fields[i][y] = new Array(width);
 		}
 	}
 }
 
 function run() {
     var pixels = image.data;
-
-	var shift = (curTime-startTime)%period/period*momentsCount;
-	var index = 0, d, moment;
+	
+	var index = 0;
+	for (var i = sources.length - 1; i >= 0; i--) {
+		sources[i].setField(fields[i], t);
+	}
     for (var y = height - 1; y >= 0; y--) {		
 		for (var x = width - 1; x >= 0; x--) {
 			var val = 0.0;
 			for (var i = sources.length - 1; i >= 0; i--) {
-				moment = D[y][x][i] - shift;
-				moment = moment >=0 ? moment : moment + momentsCount; 
-		    	val += S[moment];
+		    	val += fields[i][y][x];
 	    	}
 
 	    	pixels[index] = val;
@@ -103,41 +86,15 @@ function run() {
 		}
 	}
 	if (showSources)
-		drawSources(sources, radius);		 
+		for (var i = sources.length - 1; i >= 0; i--)
+			sources[i].draw(image);
     context.putImageData(image, 0, 0);  
-	curTime += step;
+	t += dt;
 	if (running)
 		setTimeout(run, interval);
 }
 
-function calculateSinuses(period, momentsCount) {
-	S = new Array(momentsCount);
-	var arg = 0, step = 2*Math.PI/momentsCount;
-	for(var i = momentsCount-1; i>=0; --i, arg += step) {
-		S[i] = (Math.sin(arg)+1)*Math.round((255.0-diff)/(2*sources.length));
-	}
-	return S;
-}
-
-function calculateDistances(sources) {
-	var D = new Array(height);
-	for (var y = height - 1; y >= 0; y--) {					
-		D[y] = new Array(width);
-		for (var x = width - 1; x >= 0; x--) {
-			D[y][x] = new Array(sources.length);
-			for (var i = sources.length - 1; i >= 0; i--) {
-				D[y][x][i] = Math.round(distance([x, height-y], sources[i])%period/period*momentsCount);
-			}
-		}				
-	}
-	return D;
-}
-
-function distance(a, b) {
-	return Math.sqrt((a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]));
-}
-
-function changeSources () {
+function setSources () {
 	var select = document.getElementById("select");
 	var selectedValue = select.options[select.selectedIndex].value;
 	createSources(parseInt(selectedValue, 10));
@@ -158,4 +115,75 @@ function toggleRunning() {
 function setShowSources() {
 	var checkbox = document.getElementById('checkbox');
 	showSources = checkbox.checked;
+}
+
+
+function Source(x, y, period, momentsCount, radius, sourcesCount) {
+	var self = this;
+
+	this.x = x;
+	this.y = y;
+	this.period = period;
+	this.momentsCount = momentsCount;
+	this.sourcesCount = sourcesCount;
+
+	this.radius = radius;
+
+	this.D = calculateDistance();
+	this.S = calculateSin();
+
+	function calculateDistance() {
+		var D = new Array(height);
+		for (var y = height - 1; y >= 0; y--) {					
+			D[y] = new Array(width);
+			for (var x = width - 1; x >= 0; x--) {
+				D[y][x] = Math.round(distance([x, height-y], [self.x, self.y])%period/period*momentsCount);
+			}				
+		}
+		return D;
+
+		function distance(a, b) {
+			return Math.sqrt((a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]));
+		}
+	}
+
+	function calculateSin() {
+		var S = new Array(self.momentsCount);
+		var arg = 0, step = 2*Math.PI/self.momentsCount;
+		for(var i = self.momentsCount-1; i>=0; --i, arg += step) {
+			S[i] = Math.floor(255.0*(Math.sin(arg)+1)/(2*self.sourcesCount));
+		}
+		return S;
+	}
+
+	this.draw = function(image) {
+		var pixels = image.data;
+		index = 4*(width*y + x - radius);
+		for (var j = 4*(2*radius-1); j>=0; j -= 4) {
+			pixels[index + j] = 255.0;
+	        pixels[index + j + 1] = 0;
+	        pixels[index + j + 2] = 0;
+	        pixels[index + j + 3] = pixels[index + j + 3];
+		}
+
+		index = 4*(width*(y - radius) + x);
+		for (var j = 4*width*(2*radius-1); j>=0; j -= 4*width) {
+			pixels[index + j] = 255.0;
+	        pixels[index + j + 1] = 0;
+	        pixels[index + j + 2] = 0;
+	        pixels[index + j + 3] = pixels[index + j + 3];
+		}
+	}
+
+	this.setField = function(f, t) {
+		var shift = t%period/period*momentsCount;
+		var moment;
+		for (var y = height - 1; y >= 0; y--) {		
+			for (var x = width - 1; x >= 0; x--) {
+				moment = this.D[y][x] - shift;
+				moment = moment >=0 ? moment : moment + momentsCount;
+				f[y][x] = this.S[moment]
+			}
+		}
+	}
 }
